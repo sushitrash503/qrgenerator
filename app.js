f0eb94e6958fa8fe426924cb8506ad8138ced9b6
Dcerritos <139426429+sushitrash503@users.noreply.github.com>
2024-10-16 19:05:24 -0600

    Update app.js

--- app.js
+++ app.js
@@ -6,24 +6,16 @@ const contenedorqr = document.getElementById('qrContainer');
         const historyContainer = document.getElementById('historyContainer');
         const historyList = document.getElementById('historyList');
 
-        // Recuperar historial desde localStorage o iniciar vacío
-        let qrHistory = JSON.parse(localStorage.getItem('qrHistory')) || [];
+        // Manejo del historial
+        const qrHistory = [];
 
         // Generar QR y agregarlo al historial
         generateButton.addEventListener('click', () => {
             const linkValue = linkInput.value.trim();
             if (linkValue) {
                 QR.makeCode(linkValue);
-                if (!qrHistory.includes(linkValue)) {
-                    qrHistory.push(linkValue); // Agregar la URL solo si no está en el historial
-                    updateHistory();
-                    saveHistory(); // Guardar el historial en localStorage
-                } else {
-                    nosaveHistory()
-                }
-                qrHistory.push(linkValue); // Agregar la URL al historial
+                qrHistory.push(linkValue); // Agregamos la URL al historial
                 updateHistory();
-                saveHistory(); // Guardar el historial en localStorage
             } else {
                 alert("Por favor, ingresa un enlace válido.");
             }
@@ -36,7 +28,7 @@ const contenedorqr = document.getElementById('qrContainer');
 
         // Actualizar el historial en la UI
         function updateHistory() {
-            historyList.innerHTML = ''; // Limpiar la lista antes de agregar nuevos elementos
+            historyList.innerHTML = ''; // Limpiamos la lista antes de agregar nuevos elementos
             qrHistory.forEach((link, index) => {
                 const listItem = document.createElement('li');
                 listItem.textContent = `${index + 1}: ${link}`;
@@ -44,19 +36,9 @@ const contenedorqr = document.getElementById('qrContainer');
                 
                 // Agregar evento click para insertar el valor en el input
                 listItem.addEventListener('click', () => {
-                    linkInput.value = link; // Reemplazar el valor del input con el del historial
+                    linkInput.value = link; // Reemplazamos el valor del input con el del historial
                 });
                 
-                historyList.appendChild(listItem); // Agregar el item a la lista
+                historyList.appendChild(listItem); // Agregamos el item a la lista
             });
-        }
-
-        // Guardar historial en localStorage
-        function saveHistory() {
-            localStorage.setItem('qrHistory', JSON.stringify(qrHistory));
-        }
-
-        // Inicializar historial en la carga de la página
-        window.onload = () => {
-            updateHistory();
         }
